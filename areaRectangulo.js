e7843e4989406f7b4e40808a6746f120ec4133f2
RossyD93 <rossyediaz03@gmail.com>
2023-06-05 14:34:52 -0300

    Agregar areaRectangulo

--- areaRectangulo.js
+++ areaRectangulo.js
@@ -1,7 +1,12 @@
-let base = [];
-let altura = [];
+let base = 5;
+let altura =3 ;
 
 let area = base * altura;
 
 console.log(area);
 
+base = 12.5;
+altura = 5.3;
+area = base * altura;
+
+console.log(area);

b5cd53030b329d0078a7136e650f3c180f801717
RossyD93 <rossyediaz03@gmail.com>
2023-06-05 15:33:42 -0300

    Agregar Promedio3

---
+++ promedio3.js
@@ -0,0 +1,24 @@
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+
+let promedio3 = (num1 + num2 + num3) / 3;
+console.log(promedio3);
+
+
+ num1 = -2;
+ num2 = -3;
+ num3 = -8;
+ promedio3 = (num1 + num2 + num3) / 3;
+console.log(promedio3)
+
+
+
+ num1 = -1.2;
+ num2 = 5.3;
+ num3 = -3.3;
+ promedio3 = (num1 + num2 + num3) / 3;
+console.log(promedio3);
+
+
+
